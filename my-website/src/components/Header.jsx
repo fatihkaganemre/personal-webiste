57cfefd6405dbbbffe22c5caf875c627b4120257
fatih-emre <fatih.kagan.emre@hotmail.com>
2026-03-06 13:30:34 +0100

    a bit of styling

--- my-website/src/components/Header.jsx
+++ my-website/src/components/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css';
 
 function Header() {
   return (
-    <section id="header" style={{ backgroundColor: "#405D72", color: "white" }}>
+    <section className="header">
       <div className="container col-xxl-8 px-4 py-5">
         <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
           <div className="col-lg-6">
