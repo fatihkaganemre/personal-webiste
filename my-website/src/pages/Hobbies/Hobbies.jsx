2878be71842ebdd2e4d56d79fccbc908b3f44aed
fatih-emre <fatih.kagan.emre@hotmail.com>
2026-05-11 11:35:37 +0200

    update hobbbies

--- my-website/src/pages/Hobbies/Hobbies.jsx
+++ my-website/src/pages/Hobbies/Hobbies.jsx
@@ -3,6 +3,45 @@ import { Link } from "react-router-dom";
 import HobbyCard from "./HobbyCard";
 import "./Hobbies.css";
 
+const hobbies = [
+  {
+    image: "/images/Karate.JPG",
+    title: "Karate",
+    description: (
+      <>
+        I began practicing Karate as a child at Sertdemir Karate Club in
+        Istanbul, and it has stayed with me ever since. Today I am a black belt
+        and coach at{" "}
+        <a
+          href="https://www.goatskarate.pl/"
+          target="_blank"
+          rel="noreferrer"
+          className="hobby-inline-link"
+        >
+          Goats Karate Team
+        </a>
+        {" "}
+        in Poznan. I regularly take part in international tournaments, coach
+        both kids and athletes, and I have been fortunate to share a few
+        international-level medal moments with the team.
+      </>
+    ),
+    link: "https://medium.com/@fatihkaganemre/an-insightful-approach-to-kumite-training-e0f2209558a0",
+  },
+  {
+    image: "/images/Dance.jpg",
+    title: "Dance",
+    description:
+      "I got into dancing thanks to my wife. We love spending time together dancing bachata, salsa, and kizomba, and we really enjoy going to festivals like Bachaturo, BachaMeMore, and others.",
+  },
+  {
+    image: "/images/Baglama.jpg",
+    title: "Music",
+    description:
+      "I play baglama, a traditional instrument. I love meeting my friends, singing, and making music together. It is one of my favorite ways to enjoy life.",
+  },
+];
+
 function Hobbies() {
   return (
     <div className="hobbies-page">
@@ -13,24 +52,9 @@ function Hobbies() {
         <hr />
 
         <div className="hobbies-grid">
-          <HobbyCard
-            image="/images/Karate.JPG"
-            title="Karate"
-            description="I began practicing Karate as a child at Sertdemir Karate Club in Istanbul. Since then, it has become a central part of my life. I am now a black belt and a coach at Goats Karate Team in Poznań."
-            link="https://medium.com/@fatihkaganemre/an-insightful-approach-to-kumite-training-e0f2209558a0"
-          />
-
-          <HobbyCard
-            image="/images/Dance.jpg"
-            title="Dance"
-            description="I met dancing thanks to my fiancée. We love dancing bachata, salsa and kizomba."
-          />
-
-          <HobbyCard
-            image="/images/Baglama.jpg"
-            title="Music"
-            description="I play bağlama, a traditional instrument. Music is how I disconnect and recharge."
-          />
+          {hobbies.map((hobby) => (
+            <HobbyCard key={hobby.title} {...hobby} />
+          ))}
         </div>
       </div>
     </div>
